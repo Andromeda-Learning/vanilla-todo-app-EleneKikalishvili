84165cce1246befd0035ba3912b3c87b560966ce
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-04 15:04:11 +0400

    added rename button with functionality

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ function createListElement() {
   newLi.appendChild(document.createTextNode(input.value));
   newDiv.appendChild(newLi);
   input.value = "";
+
+  //create rename button
+  let renBtn = document.createElement("button");
+  renBtn.appendChild(document.createTextNode("rename"));
+  newDiv.appendChild(renBtn);
+  renBtn.addEventListener(
+    "click",
+    () => (newLi.innerHTML = window.prompt("make changes: "))
+  );
 }
 
 form.onsubmit = e => {
