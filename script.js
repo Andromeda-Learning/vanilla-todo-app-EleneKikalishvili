9fdfe4c21f7f0f91ed607af10e1aec03923892c2
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-06 14:24:15 +0400

    added form element with input, submit button and ul element

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let input = document.getElementById("userinput");
-let ul = document.querySelector("ul");
-let button = document.getElementById("enter");
+let body = document.querySelector("body");
+
+let createform = document.createElement("form");
+body.appendChild(createform);
+
+let container = document.createElement("div");
+createform.appendChild(container);
+
+//creating input field
+let input = document.createElement("input");
+input.setAttribute("type", "text");
+input.setAttribute("placeholder", "enter items");
+container.appendChild(input);
+
+//creating submit button
+let submitElement = document.createElement("input");
+submitElement.setAttribute("type", "submit");
+container.appendChild(submitElement);
+
+//creating ul element
+let ul = document.createElement("ul");
+container.appendChild(ul);
 
 function createListElement() {
   let newDiv = document.createElement("div");
@@ -28,18 +47,8 @@ function createListElement() {
   delBtn.addEventListener("click", () => ul.removeChild(newDiv));
 }
 
-//add List after click
-button.addEventListener("click", () => {
+createform.onsubmit = e => {
   if (input.value.length > 0 && input.value.replace(/\s/g, ""))
     createListElement();
-});
-
-//add list after keypress
-input.addEventListener("keypress", event => {
-  if (
-    input.value.length > 0 &&
-    event.keyCode === 13 &&
-    input.value.replace(/\s/g, "")
-  )
-    createListElement();
-});
+  e.preventDefault();
+};
