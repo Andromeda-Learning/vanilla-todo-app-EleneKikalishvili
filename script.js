3d235b0d16e4ec86e720dc9985e7fa27d7721844
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-08 20:14:52 +0400

    replaced let with const

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let ul = document.querySelector("ul");
-let input = document.querySelector("input");
-let form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector("form");
 
 function createListElement() {
-  let newDiv = document.createElement("div");
+  const newDiv = document.createElement("div");
   ul.appendChild(newDiv);
-  let newLi = document.createElement("li");
+  const newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(input.value));
   newDiv.appendChild(newLi);
   input.value = "";
