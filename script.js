e64723e2bc05477cc059aa7cbf58d274be904a76
unknown <elene.kikalishvili.1@btu.edu.ge>
2020-03-08 17:52:56 +0400

    deleted Dynamic form, submit and input creation functionality

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-let body = document.querySelector("body");
-
-let createform = document.createElement("form");
-body.appendChild(createform);
-
-let container = document.createElement("div");
-createform.appendChild(container);
-
-//creating input field
-let input = document.createElement("input");
-input.setAttribute("type", "text");
-input.setAttribute("placeholder", "enter items");
-container.appendChild(input);
-
-//creating submit button
-let submitElement = document.createElement("input");
-submitElement.setAttribute("type", "submit");
-container.appendChild(submitElement);
-
-//creating ul element
-let ul = document.createElement("ul");
-container.appendChild(ul);
+let ul = document.querySelector("ul");
+let input = document.querySelector("input");
+let form = document.querySelector("form");
 
 function createListElement() {
   let newDiv = document.createElement("div");
   ul.appendChild(newDiv);
   let newLi = document.createElement("li");
   newLi.appendChild(document.createTextNode(input.value));
-  //append list element in newDiv
   newDiv.appendChild(newLi);
-  //clean input field
   input.value = "";
 }
 
-createform.onsubmit = e => {
+form.onsubmit = e => {
   if (input.value.length > 0 && input.value.replace(/\s/g, ""))
     createListElement();
   e.preventDefault();
